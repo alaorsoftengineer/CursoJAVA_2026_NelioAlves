e952013136944dca6c781e824e1c20758a38d2d7
Alaor Castelano <alaor.softengineer@gmail.com>
2026-06-11 18:32:11 -0300

    Reorganiza estrutura da secao 08

---
+++ secao08_Introducao_a_POO/exercicio01_triangle/Program.java
@@ -0,0 +1,34 @@
+package appexercicio1;
+import entities2.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter the rectangle width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("AREA = " + rectangle.Area());
+
+        System.out.println("PERIMETER = " + rectangle.Perimeter());
+
+        System.out.println("DIAGONAL = " + rectangle.Diagonal());
+
+
+
+
+
+
+
+        sc.close();
+
+    }
+}
